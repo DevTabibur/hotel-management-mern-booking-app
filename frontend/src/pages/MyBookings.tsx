1da04297f6f2009faabd3521abb3262cd728fd16
unknown <tobiburrohman2@gmail.com>
2024-01-11 12:36:36 +0600

    error solved

--- frontend/src/pages/MyBookings.tsx
+++ frontend/src/pages/MyBookings.tsx
@@ -14,7 +14,7 @@ const MyBookings = () => {
     return (
         <div className="space-y-5">
             <h1 className="text-3xl font-bold">My Bookings</h1>
-            {hotels.map((hotel) => (
+            {hotels?.map((hotel) => (
                 <div className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] border border-slate-300 rounded-lg p-8 gap-5">
                     <div className="lg:w-full lg:h-[250px]">
                         <img
@@ -29,7 +29,7 @@ const MyBookings = () => {
                                 {hotel.city}, {hotel.country}
                             </div>
                         </div>
-                        {hotel.bookings.map((booking) => (
+                        {hotel?.bookings?.map((booking) => (
                             <div>
                                 <div>
                                     <span className="font-bold mr-2">Dates: </span>
